ec4d660ac9a04283faa83549da960305198f9e26
Jon Martin <jchristofmartin@gmail.com>
2016-12-14 07:13:01 -0600

    adding a meshlist to the editor

--- src/app/babylon/controls/editor/scene-editor/scene-editor.component.ts
+++ src/app/babylon/controls/editor/scene-editor/scene-editor.component.ts
@@ -8,16 +8,30 @@ import { SceneService } from '../../../scene.service'
 })
 
 
+
 export class SceneEditorComponent implements OnInit {
   @ViewChild('jscolor') input;
 
-  constructor(private sceneService:SceneService) { }
+  constructor(private sceneService:SceneService) { 
+    
+    this.sceneService.sceneChangedSubscribers.push(()=>{
 
+    });
+  }
+
+ selectedMesh:any;
+ meshes:any;
   update(){
     this.sceneService.scene.clearColor = BABYLON.Color4.FromHexString("#"+this.input.nativeElement.value+"ff");
   }
 
-  ngOnInit() {}
+  onChange(value:any){
+    console.log(value);
+  }
+
+  ngOnInit() {
+    this.meshes = this.sceneService.scene.meshes;
+  }
 
   ngAfterViewInit() {
     this.input.nativeElement.value = this.sceneService.scene.clearColor.toHexString().substring(1,7);
--- src/app/babylon/scene.service.ts
+++ src/app/babylon/scene.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SceneService {
+    constructor(){
+        this.sceneChangedSubscribers = [];
+    }
+
     private _scene:BABYLON.Scene;
 
     get scene(): BABYLON.Scene {
         return this._scene;
     }
 
+    sceneChangedSubscribers:Array<()=>void>;
+    
     set scene(scene:BABYLON.Scene){
         this._scene = scene;
         this._scene.onNewMeshAddedObservable.add((eventData: BABYLON.AbstractMesh, eventState: any) => {
-            console.log("X");
+            this.sceneChangedSubscribers.forEach((element)=>{
+                element();
+            })
         });
     }
 }
